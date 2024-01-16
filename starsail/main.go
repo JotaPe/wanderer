78d6118e627e04e744e74af0fa213c72ba438092
JotaPe <jotape@protonmail.com>
2024-01-16 18:16:03 -0300

    moving pages to another subPackage

--- starsail/main.go
+++ starsail/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-        "fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/urfave/negroni"
+	"wanderer.starsail/pages"
 )
 
 func main() {
-        fmt.Println("Hello, Mom :)")
+	// Configuration Environment
+	// 0 -> Production
+	// 1 -> Dev
+	// We don't need .env file for production because we load from the
+	// docker image environment when we deploy
+	dev := os.Getenv("DEV")
+	if len(dev) >= 0 {
+		log.Println("Loaded DEV Environment!")
+		godotenv.Load("dev.env")
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", pages.Home)
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+
+	serverConfiguration := &http.Server{
+		Addr:           os.Getenv("ADDR"),
+		Handler:        n,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	log.Fatal(serverConfiguration.ListenAndServe())
 }
